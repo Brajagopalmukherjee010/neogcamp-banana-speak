b8799499cdaf490f54fe36e7f928919b8f736e8d
Brajagopalmukherjee010 <brajagopal447@gmail.com>
2021-06-10 16:02:10 +0530

    import javascript

---
+++ app.js
@@ -0,0 +1,6 @@
+// console.log("Namaste Javascript");
+// alert("Namste People");
+// prompt("Give Your username");
+var username = prompt("Give me your use name");
+var welcomeMessage = "This tricks works " + username;
+alert(welcomeMessage);
