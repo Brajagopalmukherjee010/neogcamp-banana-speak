f037ec855ca670f18d84f7efdf7c7f97f041f413
Brajagopalmukherjee010 <brajagopal447@gmail.com>
2021-06-12 11:57:32 +0530

    input output processing

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 var btnTranslator = document.querySelector("#btn-translate");
 var txtInput = document.querySelector("#txt-input");
+var outputHolder=document.querySelector("#output");
+// console.log(outputHolder);
+// outputHolder.innerText="Hi! cake walk baby"
 btnTranslator.addEventListener("click", function clickEventhandler() {
-  console.log("clicked");
-  console.log(txtInput);
-  console.log("input", txtInput.value);
+  outputHolder.innerText="jhbdfibishzvizbvzbivbsih"+txtInput.value;
+  // console.log("clicked");
+  // console.log(txtInput);
+  // console.log("input", txtInput.value);
 });
  
