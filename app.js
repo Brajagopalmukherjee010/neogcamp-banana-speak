fed6cbf023abc16ca95825f9d7d6f47694723497
Brajagopalmukherjee010 <brajagopal447@gmail.com>
2021-06-12 11:22:24 +0530

    updating

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
 var btnTranslator = document.querySelector("#btn-translate");
+var txtInput = document.querySelector("#txt-input");
 btnTranslator.addEventListener("click", function clickEventhandler() {
   console.log("clicked");
+  console.log(txtInput);
+  console.log("input", txtInput.value);
 });
+ 
